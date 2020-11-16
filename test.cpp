69300011451ae128caa4f6ccfc2f2c0a8cc6f1c2
harsathAI <harsath@tuta.io>
2020-11-16 17:53:59 -0500

    socks5h:// typofix, comments modifications

--- test.cpp
+++ test.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]){
 	// connecting to public ip check site through Tor daemon's SOCKS server and resolving the domain name remotly(Exit relay's endpoint)
 	socket_handler->connect_proxy_socks("www.ipinfo.io", 80, SOCKS5_RESOLVE::REMOTE_RESOLVE, "user", "pass"); 
 
-	// Reading the data;
+	// Sending the data(HTTP as cURL useragent);
 	std::string sample_request = "GET /ip HTTP/1.1\r\nHost: ipinfo.io\r\nUser-Agent: curl/7.65.2\r\n\r\n";
 	
 	socket_handler->write_proxy(sample_request.size(), sample_request.c_str());	
@@ -17,6 +17,7 @@ int main(int argc, char *argv[]){
 	constexpr std::size_t reply_buff_size = 2048;
 	char* read_buffer_reply =  new char[reply_buff_size];
 
+	// Reading the responce
 	socket_handler->read_proxy(reply_buff_size, read_buffer_reply);
 
 	std::cout << read_buffer_reply << std::endl;
